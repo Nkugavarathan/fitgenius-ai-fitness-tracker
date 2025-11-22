ab0ccda3d61b31a89e30e442b50e47781343b2ab
Nkugavarathan <kugavarathan28@gmail.com>
2025-11-22 21:20:12 +0530

    remove files not use

--- fitgenius-backend/src/main/java/com/example/fitgenius/model/User.java
+++ fitgenius-backend/src/main/java/com/example/fitgenius/model/User.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "users") // table name users
+@Table(name = "users")
 @Data                        //  combines @Getter, @Setter, @ToString, @EqualsAndHashCode
 @NoArgsConstructor           //  required by JPA
 @AllArgsConstructor
